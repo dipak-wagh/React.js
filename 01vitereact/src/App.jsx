ae269c2e5877b11b5b90167ceffdcbcacce31a35
Dipak Wagh <dipakwagh0502@gmail.com>
2026-03-26 18:43:24 +0530

    created own react library and jsx

--- 01vitereact/src/App.jsx
+++ 01vitereact/src/App.jsx
@@ -1,15 +1,13 @@
 import Chai from "./chai"
 function App() {
+  const username = "chai aur code"
   
   return (
-    <>
+  <>
   <Chai/>
-  <h1>Hello</h1>
+  <h1>chai aur react {username} </h1>
   <p>Test React Project</p>
-
-    </>
-
-
+  </>
   )
 }
 
